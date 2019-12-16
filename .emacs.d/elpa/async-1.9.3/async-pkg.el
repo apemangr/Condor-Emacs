88e0dd06f19cf8858e8ad42b04cf8561fd636675
Guillermo Rojas <apemangr@gmail.com>
2019-12-16 00:07:57 -0300

    Emacs files

---
+++ .emacs.d/elpa/async-1.9.3/async-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from async.el  -*- no-byte-compile: t -*-
+(define-package "async" "1.9.3" "Asynchronous processing in Emacs" '((cl-lib "0.5") (nadvice "0.3")) :keywords '("async") :authors '(("John Wiegley" . "jwiegley@gmail.com")) :maintainer '("John Wiegley" . "jwiegley@gmail.com") :url "https://github.com/jwiegley/emacs-async")
---
+++ .emacs.d/init.el
@@ -0,0 +1,27 @@
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
+ '(frame-resize-pixelwise t)
+ '(inhibit-startup-echo-area-message "apeman")
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(package-archives
+   (quote
+    (("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (auto-package-update imenu-list centaur-tabs ccls yasnippet helm use-package doom-modeline doom-themes)))
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "SF Mono" :foundry "APPL" :slant normal :weight normal :height 110 :width normal)))))
